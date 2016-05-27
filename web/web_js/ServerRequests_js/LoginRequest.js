80f0e78a9451b6910399704657558aaeef0be87e
Vincent-20100 <bessouet.vincent@gmail.com>
2016-05-27 12:10:46 +0100

    send request to distant server *not tested: impossible to reach the server*

--- web/web_js/ServerRequests_js/LoginRequest.js
+++ web/web_js/ServerRequests_js/LoginRequest.js
@@ -1,14 +1,5 @@
-<script language="javascript" type="text/javascript">
-
-	//constuctor
-	function LoginRequest(username, password){
-		this.username = username;
-		this.password = password;
-	}
-
-	module.exports = LoginRequest;
-</script>
-
-<noscript>
-	<h3> This web site requires JavaScript</h3>
-</noscript>
+//constuctor
+function LoginRequest(username, password){
+	this.username = username;
+	this.password = password;
+}
--- web/web_js/ServerRequests_js/ServerRequestBuilder.js
+++ web/web_js/ServerRequests_js/ServerRequestBuilder.js
@@ -1,31 +1,25 @@
-<script language="javascript" type="text/javascript">
+//This object must extends/ HttpRequestBuilder class
+//constuctor
+/*
+function ServerRequestBuilder(){
+	this = new HttpRequestBuilder();
+}
 
-	//This object must extends/ HttpRequestBuilder class
-	//constuctor
-	function ServeurRequestBuilder(){
+//Methods
+//Both function must extends HttpRequestBuilder
+ServerRequestBuilder.prototype.newRequest = function(){
+	this.json.setOutputType();
+	// Allow zero valued properties to be outputted
+	this.json.setUsePrototypes(false);
+	return this.newRequest();
+}
 
-	}
-
-	//Methods
-	//Both function must extends HttpRequestBuilder
-	ServeurRequestBuilder.prototype.newRequest = function(){
-		json.setOutputType();
-		// Allow zero valued properties to be outputted
-		json.setUsePrototypes(false);
-		return super.newRequest();
-	}
-
-	ServeurRequestBuilder.prototype.jsonContent = function(content){
-		// Uncomment this line to print each JSON object sent to the server
-		//Gdx.app.log("debug", json.prettyPrint(content));
-		if(typeof content != "undefined")
-			return super.jsonContent(content);
-		else
-			return super.jsonContent();
-	}
-
-</script>
-
-<noscript>
-	<h3> This web site requires JavaScript</h3>
-</noscript>
+ServerRequestBuilder.prototype.jsonContent = function(content){
+	// Uncomment this line to print each JSON object sent to the server
+	//Gdx.app.log("debug", json.prettyPrint(content));
+	if(typeof content != "undefined")
+		return this.jsonContent(content);
+	else
+		return this.jsonContent();
+}
+*/
