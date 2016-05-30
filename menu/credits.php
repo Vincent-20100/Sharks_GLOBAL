d19fed2c4c7387f220eed21fb98079e000c7a7b9
argioss <tristan.le.nair@gmail.com>
2016-05-30 15:04:20 +0100

    t

---
+++ menu/credits.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="css/menu.css" />
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+		<!-- Latest compiled JavaScript -->
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<title>Shark Tagging Game</title>
+	</head>
+	<body background="images/scores.jpg">
+		<div id="devant">
+			<h2> <font color = blue>
+				Credits
+			</h2>
+			<br><br><font color = #483D8B>
+			<p>Tristan Le Nair</p>			
+			<p>Florian Talour</p>
+			<p>Vincent Bessouet</p>
+			<p>Cassien Ippolito</p>
+			
+			<div class="menu_simple">
+					<ul class="nav nav-pills nav-stacked list-group">
+						<li><a class="col-sm-12 list-group-item" href="menu.html">Back</a></li>
+					</ul>
+				</div>
+		</div>
+	</body>
+</html>
---
+++ menu/menu.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="css/menu.css" />
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+		<!-- Latest compiled JavaScript -->
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<title>Shark Tagging Game</title>
+	</head>
+	<body background="images/image_2.png">
+		<div id="devant">
+			<div class="raw">
+				<h1> <font color = blue>
+					Shark Tagging Game
+				</h1>
+				<div class="menu_simple">
+					<ul class="nav nav-pills nav-stacked list-group">
+						<li><a class="col-sm-12 list-group-item" href="#">Play</a></li>
+						<li><a class="col-sm-12 list-group-item" href="#">Tutorial</a></li>
+						<li><a class="col-sm-12 list-group-item" href="scores.html">Highscores</a></li>
+						<li><a class="col-sm-12 list-group-item" href="credits.html">Credits</a></li>
+						<li><a class="col-sm-12 list-group-item" href="#">Log out</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
