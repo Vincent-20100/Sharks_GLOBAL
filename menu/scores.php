a1d5bdb7c35debdfabab06e8099d0346aacb8149
argioss <tristan.le.nair@gmail.com>
2016-05-31 11:00:09 +0100

    scores

---
+++ menu/scores.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="css/menu.css" />
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+		<!-- Latest compiled JavaScript -->
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<title>Shark Tagging Game</title>
+	</head>
+	<body background="images/scores.jpg">
+		<div id="devant">
+			<div class="container">
+				<h2> <font color = blue>
+					Highscores
+				</h2>
+				<table width=30% border=1 class="table-bordered">
+					<tr>
+						<th width=60%>Player</td>
+						<th width=40%>Score</td>
+					</tr>
+					<tr>
+						<td width=60%>play1</td>
+						<td width=40%>55</td>
+					</tr>
+					<tr>
+						<td width=60%>play2</td>
+						<td width=40%>45</td>
+					</tr>
+				</table>
+				<div class="menu_simple">
+					<ul class="nav nav-pills nav-stacked list-group">
+						<li><a class="col-sm-12 list-group-item" href="menu.php">Back</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
+
